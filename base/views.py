356e16604cd7b4ff41c99795f2a3505fd8fa737e
AGranjaL <aaron.granja.l@gmail.com>
2022-01-03 23:29:08 +0100

    new django project for gym lovers

---
+++ base/views.py
@@ -0,0 +1,51 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Room, Message, Topic
+from .forms import RoomForm
+# Create your views here.
+
+#rooms = [
+#    {'id': '1', 'name': 'CrossFit'},
+#    {'id': '2', 'name': 'Weighlifting'},
+#    {'id': '3', 'name': 'Bodybuilding'},
+#]
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    messages = Message.objects.filter(room=room)
+    context = {'room': room, 'messages': messages}
+    return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+    
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    context = {'obj': room}
+    return render(request, 'base/delete.html', context)
