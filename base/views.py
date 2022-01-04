495cd1f5fcd4f8df9979f6de9383f35404ea628f
AGranjaL <aaron.granja.l@gmail.com>
2022-01-04 10:42:51 +0100

    search bar

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Message, Topic
 from .forms import RoomForm
 # Create your views here.
@@ -7,13 +8,21 @@ from .forms import RoomForm
 def home(request):
     
     q = request.GET.get('q')
-    #if q is not None then filter the rooms if they have messages that contain the topic q
-    rooms = Room.objects.filter(message__topic__name__icontains=q) if request.GET.get('q') != None else Room.objects.all()
+    #if q is not None or not '' then filter the rooms if they have messages that contain the topic q
+    rooms = Room.objects.filter(Q(message__topic__name__icontains=q) |
+    Q(name__icontains=q) |
+    Q(description__icontains=q)) if request.GET.get('q') != None and request.GET.get('q') != '' else Room.objects.all()
 
     
     #rooms = Room.objects.all() #all() returns a QuerySet with all the rooms in the database
     topics = Topic.objects.all()
-    context = {'rooms': rooms, 'topics': topics}
+    room_count = rooms.count()
+    #get the number of messages in each room
+    room_messages = {}
+    for room in rooms:
+        room_messages[room.id] = Message.objects.filter(room=room).count() 
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
